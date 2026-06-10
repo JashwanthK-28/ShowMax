972e3d02ffb748000d7806a443632cd270c7fdcd
Jashwanth <jashwanthkumarbolla@gmail.com>
2026-06-10 20:29:06 +0530

    feat: update site logo and favicon with new stylistic design elements and completed navbar section

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 import { assets } from "../assets/assets";
-import { Link } from "react-router-dom";
-import { SearchIcon, MenuIcon, XIcon } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
+import { SearchIcon, MenuIcon, XIcon, TicketPlus } from "lucide-react";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const { user } = useUser();
+  const { openSignIn } = useClerk();
+
+  const navigate = useNavigate();
+
   return (
     <div className="fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5">
-      <Link to="/" className="max-md:flex-1">
+      <Link to="/" className="flex-1">
         <img src={assets.logo} alt="logo" className="w-36 h-auto" />
       </Link>
 
@@ -60,11 +66,26 @@ const Navbar = () => {
         </Link>
       </div>
 
-      <div className="flex items-center justify-between gap-8">
+      <div className="flex md:flex-1 items-center justify-end gap-6 md:gap-8">
         <SearchIcon className="max-md:hidden w-6 h-6 cursor-pointer" />
-        <button className="px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer">
-          Login
-        </button>
+        {!user ? (
+          <button
+            onClick={openSignIn}
+            className="px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer"
+          >
+            Login
+          </button>
+        ) : (
+          <UserButton>
+            <UserButton.MenuItems>
+              <UserButton.Action
+                label="My Bookings"
+                labelIcon={<TicketPlus width={15} />}
+                onClick={() => navigate("/my-bookings")}
+              />
+            </UserButton.MenuItems>
+          </UserButton>
+        )}
       </div>
 
       <MenuIcon
