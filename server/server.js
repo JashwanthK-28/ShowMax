c1524e04e5f17b7bd1afa477510d6302ad4fb835
Jashwanth <jashwanthkumarbolla@gmail.com>
2026-06-22 15:44:45 +0530

    feat: implement Clerk authentication, migrate admin access, and add user-specific controllers for bookings and favorites.

--- server/server.js
+++ server/server.js
@@ -7,6 +7,9 @@ import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
 import showRouter from "./routes/showRoutes.js";
+import bookingRouter from "./routes/bookingRoutes.js";
+import adminRouter from "./routes/adminRoutes.js";
+import userRouter from "./routes/userRouter.js";
 
 const app = express();
 const port = process.env.PORT || 5001;
@@ -18,12 +21,15 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use(clerkMiddleware());
+app.use(clerkMiddleware({ clockSkewInMs: 60000 }));
 
 app.get("/", (req, res) => res.send("Server is Live..."));
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/show", showRouter);
+app.use("/api/booking", bookingRouter);
+app.use("/api/admin", adminRouter);
+app.use("/api/user", userRouter);
 
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
